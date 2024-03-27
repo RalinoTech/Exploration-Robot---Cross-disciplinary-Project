ca1d67bfd2a8b9f51f466925c1c43407481ce90d
Melanie <melanie.mariotte-garcia@cpe.fr>
2024-03-27 09:32:01 +0100

    vitesse

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
-const validKeys = ['q', 's', 'd', 'z'];
-const display = document.getElementById('keyPressed');
-let pressedKeys = []; // Liste des touches actuellement enfoncées
+// Gestion des touches Q, S, D et Z
+const arrowKeys = ['q', 's', 'd', 'z'];
+let arrowPressedKeys = []; // Liste des touches Q, S, D et Z actuellement enfoncées
 
 document.addEventListener('keydown', function (event) {
     const key = event.key.toLowerCase();
-    if (validKeys.includes(key) && !pressedKeys.includes(key)) {
-        pressedKeys.push(key); // Ajoute la touche actuellement enfoncée à la liste
+    if (arrowKeys.includes(key) && !arrowPressedKeys.includes(key)) {
+        arrowPressedKeys.push(key); // Ajoute la touche actuellement enfoncée à la liste
         document.getElementById('key' + key.toUpperCase()).classList.add('key-pressed');
         sendData(key); // Envoie la touche enfoncée au serveur Flask
     }
@@ -13,10 +13,30 @@ document.addEventListener('keydown', function (event) {
 
 document.addEventListener('keyup', function (event) {
     const key = event.key.toLowerCase();
-    if (pressedKeys.includes(key)) {
-        pressedKeys = pressedKeys.filter(k => k !== key); // Retire la touche relâchée de la liste
-        display.style.color = '#333';
+    if (arrowPressedKeys.includes(key)) {
+        arrowPressedKeys = arrowPressedKeys.filter(k => k !== key); // Retire la touche relâchée de la liste
         document.getElementById('key' + key.toUpperCase()).classList.remove('key-pressed');
+    }
+});
+
+const speedKeys = ['1', '2', '3'];
+let activeSpeed = null;
+
+document.addEventListener('keydown', function (event) {
+    const key = event.key;
+    if (speedKeys.includes(key)) {
+        // Si la touche actuelle est déjà active, la désactiver
+        if (activeSpeed === key) {
+            document.getElementById('key' + key).classList.remove('key-pressed');
+            activeSpeed = null;
+        } else { // Sinon, activer la touche actuelle et désactiver la touche précédemment activée
+            if (activeSpeed) {
+                document.getElementById('key' + activeSpeed).classList.remove('key-pressed');
+            }
+            document.getElementById('key' + key).classList.add('key-pressed');
+            activeSpeed = key;
+            sendData(key); // Envoie la touche enfoncée au serveur Flask
+        }
     }
 });
 
