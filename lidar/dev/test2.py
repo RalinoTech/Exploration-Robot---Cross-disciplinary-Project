469fede9967aacce5057d1a1541cb89d756f270b
Darksasuke02 <kevin.burnot@cpe.fr>
2024-04-02 17:28:14 +0200

    detek obsctacles fonctionnent bien

--- lidar/dev/test2.py
+++ lidar/dev/test2.py
@@ -6,7 +6,7 @@ from adafruit_rplidar import RPLidar
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0' #'/dev/ttyUSB0'
 #lidar = RPLidar(None, PORT_NAME, timeout=3)
-lidar = RPLidar(None, PORT_NAME, timeout=50)
+lidar = RPLidar(None, PORT_NAME, timeout=3)
 
 # used to scale data to fit on the screen
 max_distance = 4000
@@ -20,14 +20,14 @@ try:
 #    print(lidar.get_info())
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-        
-
-        for i in range(len(scan_data)):
-            if scan_data[i]<=300 and scan_data[i]!=-1:
+            ang=min([359, floor(angle)])
+            scan_data[ang] = distance
+            if ang>180 and ang<360 and distance<300 and distance!=-1:
                 print("ARRA LES CONDéES")
             else:
                 print("je roule")
+
+    
 
 except KeyboardInterrupt:
     print('Stopping.')
