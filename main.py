3ab7febef108a3c138e6c13deeee0ebb7d3a4200
Melanie <melanie.mariotte-garcia@cpe.fr>
2024-04-02 16:22:30 +0200

    changement touche

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+from robotlib import Server
 
 app = Flask(__name__)
 
 @app.route('/input', methods=['GET'])
 def handle_input():
     inp = request.args.get('inp')
-    print("Touche enfoncée :", inp)
+    server.send_input(inp)
     return 'confirmation'  
 
 @app.route('/')
@@ -13,4 +14,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    server = Server("192.168.166.148", 7070)
+    server.connect()
     app.run(debug=True)
