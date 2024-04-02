7d08dea7c1fb501da8671d62857e18ef83934909
Darksasuke02 <kevin.burnot@cpe.fr>
2024-04-02 16:57:11 +0200

    maj test detek obstacles

--- lidar/dev/test2.py
+++ lidar/dev/test2.py
@@ -4,9 +4,9 @@ from adafruit_rplidar import RPLidar
 
 
 # Setup the RPLidar
-PORT_NAME = 'COM9' #'/dev/ttyUSB0'
+PORT_NAME = '/dev/ttyUSB0' #'/dev/ttyUSB0'
 #lidar = RPLidar(None, PORT_NAME, timeout=3)
-lidar = RPLidar(None, PORT_NAME, timeout=3)
+lidar = RPLidar(None, PORT_NAME, timeout=50)
 
 # used to scale data to fit on the screen
 max_distance = 4000
@@ -21,7 +21,13 @@ try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-        process_data(scan_data)
+        
+
+        for i in range(len(scan_data)):
+            if scan_data[i]<=300 and scan_data[i]!=-1:
+                print("ARRA LES CONDéES")
+            else:
+                print("je roule")
 
 except KeyboardInterrupt:
     print('Stopping.')
