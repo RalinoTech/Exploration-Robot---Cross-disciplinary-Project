fd64d4bdb465dfc07bcc27d65763a87d7f612a6e
Darksasuke02 <kevin.burnot@cpe.fr>
2024-04-30 14:30:26 +0200

    maj transmission data lidar bug dans lib lidar

--- robot/CourseLidarlib.py
+++ robot/CourseLidarlib.py
@@ -6,25 +6,22 @@ class CourseLidar:
         self.lidar = RPLidar(None, port_name, timeout=timeout)
         self.max_distance = 4000
         self.scan_data = [-1] * 360
+        self.last_scan_data = None
 
-    def recup_une_valeur(self):
-        compteur=0
-        try:
-            for scan in self.lidar.iter_scans():
-                compteur=compteur+1
-                for (_, angle, distance) in scan:
-                    ang = min([359, floor(angle)])
-                    self.scan_data[ang] = distance
-                if compteur==5:
-                    return(self.scan_data)
-                
+    def update_scan_data(self):
+        for scan in self.lidar.iter_scans():
+            for (_, angle, distance) in scan:
+                ang = min([359, floor(angle)])
+                self.scan_data[ang] = distance
+            self.last_scan_data = self.scan_data[:]  # Copy the data to last_scan_data
+            yield
 
-        except KeyboardInterrupt:
-            print('Stopping.')
+    def get_last_scan_data(self):
+        return self.last_scan_data
 
-        
-
-
-if __name__ == "__main__":
-    print("[x] This code is a module!")
-    exit(-1)
+# Example usage:
+#lidar = GPTLidar()
+#lidar.update_scan_data()  # Start scanning
+#Do something else here, or sleep for a while
+#last_scan_data = lidar.get_last_scan_data()  # Get the last scan data
+#print(last_scan_data)  # Or do something with the data
--- server/main.py
+++ server/main.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     
     server = Server(sys.argv[1], int(sys.argv[2]))
     server.connect()
-    print(server.rnp())
+    while(1):
+        print(server.rnp())
     app.run(debug=False, port=5000, use_reloader=False)
     
