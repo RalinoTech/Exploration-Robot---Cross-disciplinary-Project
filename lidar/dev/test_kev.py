9483e397113bc56b5de7149431855e9bd5e63c18
Melanie <melanie.mariotte-garcia@cpe.fr>
2024-05-15 12:08:32 +0200

    Merge remote-tracking branch 'origin/lidar' into dev

---
+++ lidar/dev/test_kev.py
@@ -0,0 +1,36 @@
+import os
+from math import floor
+from adafruit_rplidar import RPLidar
+
+
+# Setup the RPLidar
+PORT_NAME = '/dev/ttyUSB0' #'/dev/ttyUSB0'
+#lidar = RPLidar(None, PORT_NAME, timeout=3)
+lidar = RPLidar(None, PORT_NAME, timeout=3)
+
+# used to scale data to fit on the screen
+max_distance = 4000
+
+def process_data(data):
+    print(data)
+
+scan_data = [-1]*360
+
+try:
+#    print(lidar.get_info())
+    for scan in lidar.iter_scans():
+        for (_, angle, distance) in scan:
+            ang=min([359, floor(angle)])
+            scan_data[ang] = distance
+            #if ang>180 and ang<360 and distance<300 and distance!=-1:
+                #print("ARRA LES CONDéES")
+            #else:
+                #print("je roule")
+        process_data(scan_data)
+
+except KeyboardInterrupt:
+    print('Stopping.')
+
+lidar.stop()
+lidar.disconnect()
+
